6fe6b5928809c889f1599ab8742f075b0527f323
Guilherme <mttsguil@gmail.com>
2022-10-17 08:35:36 -0300

    add: page PublicPerfil

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -4,6 +4,8 @@ import HomePage from '../Pages/Homepages/Homepage';
 import SingUp from '../Pages/SingUp And Login/SingUp/SingUp';
 import UserInformation from '../Pages/UserInformation'
 import MyProfile from '../Pages/MyProfile';
+import MyProfilePublic from '../Pages/MyProfilePublic'
+import EditProfile from '../Pages/EditProfile'
 
 
 const RouterApp = () => (
@@ -13,6 +15,8 @@ const RouterApp = () => (
       <Route path='/singup' element={<SingUp/>} />
       <Route path='/userInformation' element={<UserInformation/>} />
       <Route path='/MyProfile' element={<MyProfile/>} />
+      <Route path='/MyProfilePublic' element={<MyProfilePublic/>} />
+      <Route path='/EditProfile' element={<EditProfile/>} />
     </Routes>
   </Router>
 );
