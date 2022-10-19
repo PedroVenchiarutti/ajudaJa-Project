6890e8466243ce2705e921c30c077d6a52398822
Smooke09 <pedrolucas1014zz@gmail.com>
2022-10-19 08:27:44 -0300

    api ajust

--- src/Pages/ChatBox/index.jsx
+++ src/Pages/ChatBox/index.jsx
@@ -1,75 +1,88 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import SendIcon from '@mui/icons-material/Send';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import {chatPreview, botMessage, chatMessage, messageArea, titleChat} from './classTailwind'
+import io from 'socket.io-client';
 
-const Chatbox = props => { 
+import {
+  chatPreview,
+  botMessage,
+  chatMessage,
+  messageArea,
+  titleChat,
+} from './classTailwind';
 
-    const [textMessage, setTextMessage] = useState('')
-    const [closeWindow, setCloseWindow] = useState(true)
-    const [myArray, updateMyArray] = useState([])
+const socket = io('http://localhost:3333');
+// definindo a sala
+const room = 'room1';
 
-    const bottomRef = useRef(null);
-    
+const Chatbox = (props) => {
+  const [textMessage, setTextMessage] = useState('');
+  const [closeWindow, setCloseWindow] = useState(true);
+  const [myArray, updateMyArray] = useState([]);
 
-    const onClick = () => { 
-        updateMyArray(arr => [...arr, `${textMessage}`])
-        setTextMessage('')
+  const bottomRef = useRef(null);
+
+  const onClick = () => {
+    updateMyArray((arr) => [...arr, `${textMessage}`]);
+
+    // Envinado a msg para o backend
+    socket.emit('chat_message', {
+      room,
+      message: textMessage,
+    });
+
+    setTextMessage('');
+  };
+
+  const keyHandler = (e) => {
+    if (e.key === 'Enter') {
+      return onClick();
+    } else if (e.key === 'Escape') {
+      setTextMessage('');
     }
+  };
 
-    const keyHandler = e => { 
-        if(e.key === 'Enter') { 
-            return onClick()
-        } else if(e.key === 'Escape') { 
-            setTextMessage('')
-        }
-    }
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [textMessage]);
 
-    useEffect(()=> { 
-        bottomRef.current?.scrollIntoView({behavior: 'smooth'})
-    }, [textMessage])
+  return (
+    <div className={chatPreview(closeWindow)}>
+      <div className={titleChat(closeWindow)}>
+        <h3 className="font-bold">{closeWindow ? `` : `ChatBot - Online`}</h3>
 
-
-    return(
-       <div className={chatPreview(closeWindow)}>
-
-        <div className={titleChat(closeWindow)}>
-            <h3 className="font-bold">
-               {closeWindow ? `` : `ChatBot - Online`} 
-                </h3>
-
-
-        <button onClick={_ => setCloseWindow(!closeWindow)}>
-            
-            {closeWindow ? <ArrowUpwardIcon/> :   <ArrowDownwardIcon/> }
-          
-        
+        <button onClick={(_) => setCloseWindow(!closeWindow)}>
+          {closeWindow ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />}
         </button>
-        </div>
+      </div>
 
-        <div className={closeWindow ? 'hidden' : ''}>
-            <div className={messageArea}>
-                <div className={botMessage}>
-                    Bom dia, no que posso ajudar?
-                </div>
-                {myArray.map(e =>
-                <div className={chatMessage}>
-                    {e}
+      <div className={closeWindow ? 'hidden' : ''}>
+        <div className={messageArea}>
+          <div className={botMessage}>Bom dia, no que posso ajudar?</div>
+          {myArray.map((e) => (
+            <div className={chatMessage}>
+              {e}
 
-                    <div ref={bottomRef}> </div>
-                    </div>
-                )}
+              <div ref={bottomRef}> </div>
             </div>
-            <div className="fixed h-[30px] m-2 flex gap-2 pb-1">
-            <input className="pl-1 rounded-lg drop-shadow-md " type="text" value={textMessage} onKeyUp={e => keyHandler(e)} onChange={e => setTextMessage(e.target.value)} />
-            <button onClick={onClick} className="text-navFontColor">
-                <SendIcon/>
-            </button>
-            </div>
+          ))}
         </div>
-       </div>
-    )
-}
+        <div className="fixed h-[30px] m-2 flex gap-2 pb-1">
+          <input
+            className="pl-1 rounded-lg drop-shadow-md "
+            type="text"
+            value={textMessage}
+            onKeyUp={(e) => keyHandler(e)}
+            onChange={(e) => setTextMessage(e.target.value)}
+          />
+          <button onClick={onClick} className="text-navFontColor">
+            <SendIcon />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chatbox
+export default Chatbox;
