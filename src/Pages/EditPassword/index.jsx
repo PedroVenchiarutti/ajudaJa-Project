dc0535c7ab3b84d0a1fb4a66b945822bf6b18212
demershow <demersontorres520@gmail.com>
2022-11-01 11:40:19 -0300

    Errors on changePassword scrren handled

--- src/Pages/EditPassword/index.jsx
+++ src/Pages/EditPassword/index.jsx
@@ -3,17 +3,17 @@ import Modal from '../../Components/Modal';
 import Fade from 'react-reveal/Fade';
 import Api from '../../Api/api';
 import { Link } from 'react-router-dom';
-import InputPassword from '../../Components/inputPassword'
+import InputPassword from '../../Components/inputPassword';
+import { loadingAlert, loginHandler } from '../../Components/alerts';
 
 const editPassword = () => {
-
   const [client, setClient] = useState({
     password: '',
     newPassword: '',
     confirmPassword: '',
     showOldPassword: false,
     showNewPassword: false,
-    showRepeatPassword: false
+    showRepeatPassword: false,
   });
 
   const [openModal, setOpenModal] = useState(false);
@@ -50,81 +50,93 @@ const editPassword = () => {
   };
 
   const handleButton = (e) => {
+    loadingAlert();
 
     Api.patch(
       `/private/update/users/password/${id}`,
       {
         password: client.password,
         newPassword: client.newPassword,
-        confirmPassword: client.confirmPassword
+        confirmPassword: client.confirmPassword,
       },
-      config
+      config,
     )
       .then((response) => {
         console.log(response);
+        loginHandler({
+          icon: 'success',
+          title: 'Senha alterada com sucesso!',
+        });
       })
       .catch((error) => {
+        loginHandler({
+          icon: 'error',
+          title: error.response.data.message,
+        });
         console.log(error);
       });
   };
 
- 
   return (
     <div className="w-full   bg-white">
       {/* Content */}
       <div className="lg:w-[1080px] pt-32 pb-32 grid m-auto">
         <Fade bottom>
           <div className=" bg-[#fff] drop-shadow-lg  grid-cols-2 m-auto lg:px-16 py-5 rounded-lg pb-10 px-4">
-            
             <div className="pt-4 formAndButtons flex flex-col  content-center ">
-              <h2 className='font-bold text-xl pb-2 '>Altere sua senha</h2>
+              <h2 className="font-bold text-xl pb-2 ">Altere sua senha</h2>
               <form className="flex flex-col gap-5 items-center ">
-
-                <InputPassword 
-                titleLabel='Senha atual'
-                type={client.showOldPassword ? 'text' : 'password'}
-                value={client.password}
-                onChange={handleChange('password')}
-                onClick={handleClickShowPassword('showOldPassword')}
-                onMouseDown={handleMouseDownPassword}
-                visibility={client.showOldPassword}
-                onKeyUp={keyHandler}/>
-
-                <InputPassword 
-                titleLabel='Senha nova'
-                type={client.showNewPassword ? 'text' : 'password'}
-                value={client.newPassword}
-                onChange={handleChange('newPassword')}
-                onClick={handleClickShowPassword('showNewPassword')}
-                onMouseDown={handleMouseDownPassword}
-                visibility={client.showNewPassword}
-                onKeyUp={keyHandler}/>
+                <InputPassword
+                  titleLabel="Senha atual"
+                  type={client.showOldPassword ? 'text' : 'password'}
+                  value={client.password}
+                  onChange={handleChange('password')}
+                  onClick={handleClickShowPassword('showOldPassword')}
+                  onMouseDown={handleMouseDownPassword}
+                  visibility={client.showOldPassword}
+                  onKeyUp={keyHandler}
+                />
 
                 <InputPassword
-                titleLabel='Repita senha nova'
-                type={client.showRepeatPassword ? 'text' : 'password'}
-                value={client.confirmPassword}
-                onChange={handleChange('confirmPassword')}
-                onClick={handleClickShowPassword('showRepeatPassword')}
-                onMouseDown={handleMouseDownPassword}
-                visibility={client.showRepeatPassword}
-                onKeyUp={keyHandler}
-                 />
+                  titleLabel="Senha nova"
+                  type={client.showNewPassword ? 'text' : 'password'}
+                  value={client.newPassword}
+                  onChange={handleChange('newPassword')}
+                  onClick={handleClickShowPassword('showNewPassword')}
+                  onMouseDown={handleMouseDownPassword}
+                  visibility={client.showNewPassword}
+                  onKeyUp={keyHandler}
+                />
 
-                
+                <InputPassword
+                  titleLabel="Repita senha nova"
+                  type={client.showRepeatPassword ? 'text' : 'password'}
+                  value={client.confirmPassword}
+                  onChange={handleChange('confirmPassword')}
+                  onClick={handleClickShowPassword('showRepeatPassword')}
+                  onMouseDown={handleMouseDownPassword}
+                  visibility={client.showRepeatPassword}
+                  onKeyUp={keyHandler}
+                />
               </form>
 
               <div className="buttons py-6 pt-14 flex flex-col gap-2 justify-center">
-              <Link to=""> <button
-                  className="border px-8 py-2 rounded-lg w-[100%] text-navFontColor font-bold  hover:bg-navBg hover:text-white"
-                onClick={e => handleButton()}
-                >
-                  Salvar
-                </button></Link>
-               
-                <Link to="/editprofile"> <button className="px-6 py-2 w-[100%] rounded-lg text-white font-bold  bg-navBg hover:bg-opacity-0 border hover:text-navFontColor hover:border ">
-                  Voltar
-                </button></Link>
+                <Link to="">
+                  {' '}
+                  <button
+                    className="border px-8 py-2 rounded-lg w-[100%] text-navFontColor font-bold  hover:bg-navBg hover:text-white"
+                    onClick={(e) => handleButton()}
+                  >
+                    Salvar
+                  </button>
+                </Link>
+
+                <Link to="/editprofile">
+                  {' '}
+                  <button className="px-6 py-2 w-[100%] rounded-lg text-white font-bold  bg-navBg hover:bg-opacity-0 border hover:text-navFontColor hover:border ">
+                    Voltar
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
