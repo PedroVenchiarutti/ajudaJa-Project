6c51e5700f18461fa39642c5f988433b0ca24e42
demershow <demersontorres520@gmail.com>
2022-11-09 10:51:17 -0300

    fix: apiConn

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-import axios from 'axios';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
