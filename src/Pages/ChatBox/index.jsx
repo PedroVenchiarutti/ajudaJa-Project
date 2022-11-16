15ad6c4becb70eb1370933b04350e37b54e3822b
demershow <demersontorres520@gmail.com>
2022-11-16 11:13:42 -0300

    fix apiCon

--- src/Pages/ChatBox/index.jsx
+++ src/Pages/ChatBox/index.jsx
@@ -16,10 +16,8 @@ import {
 import CircleIcon from '@mui/icons-material/Circle';
 import Message from '../../Components/Message';
 
-
 /* const socket = io('http://localhost:3333'); */
 // const socket = io(import.meta.env.VITE_URL_CONNECTION);
-
 
 const Chatbox = (props) => {
   const [emailVerific, setEmailVerific] = useState(false);
@@ -146,143 +144,148 @@ const Chatbox = (props) => {
         .catch((error) => {
           console.log(error);
         });
-  // Para atualiszar as msg
-  useEffect(() => {
-    socket.on('message_new', (newdata) => {
-      if (newdata == null) return;
-      setMsg((msg) => [...msg, newdata]);
-    });
-    socket.on('message_bot', (data) => {
-      if (data == null) return;
-      setMsg((msg) => [...msg, data]);
-    });
-  }, [setMsg]);
+      // Para atualiszar as msg
+      useEffect(() => {
+        socket.on('message_new', (newdata) => {
+          if (newdata == null) return;
+          setMsg((msg) => [...msg, newdata]);
+        });
+        socket.on('message_bot', (data) => {
+          if (data == null) return;
+          setMsg((msg) => [...msg, data]);
+        });
+      }, [setMsg]);
 
-  const keyHandler = (e, f) => {
-    if (e.key === 'Enter') {
-      return onClick();
-    } else if (e.key === 'Escape') {
-      setTextMessage('');
-    }
-  };
+      const keyHandler = (e, f) => {
+        if (e.key === 'Enter') {
+          return onClick();
+        } else if (e.key === 'Escape') {
+          setTextMessage('');
+        }
+      };
 
-  useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [msg]);
+      useEffect(() => {
+        bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+      }, [msg]);
 
-  const initiateChat = () => {
-    if (!/^[A-Za-z0-9]{4,255}$/.test(name)) {
-      setInitChat(true);
-      setUserVerific(true);
-      console.log('if');
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      setInitChat(true);
-      setEmailVerific(true);
-      console.log('if 2');
-    } else {
-      setInitChat(false);
-      setUserVerific(false);
-      setEmailVerific(false);
-      socket.connect();
-      startChat();
-    }
-  };
+      const initiateChat = () => {
+        if (!/^[A-Za-z0-9]{4,255}$/.test(name)) {
+          setInitChat(true);
+          setUserVerific(true);
+          console.log('if');
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+          setInitChat(true);
+          setEmailVerific(true);
+          console.log('if 2');
+        } else {
+          setInitChat(false);
+          setUserVerific(false);
+          setEmailVerific(false);
+          socket.connect();
+          startChat();
+        }
+      };
 
-  return (
-    <div className={chatPreview(closeWindow)}>
-      <div className={titleChat(closeWindow)}>
-        <div className="flex gap-2 items-center">
-          <h3 className="font-bold">
-            {closeWindow ? `` : `ChatBot - Online `}
-          </h3>
-          {closeWindow ? null : (
-            <CircleIcon
-              sx={{ fontSize: '10px', color: '#6DE040' }}
-              className="animate-pulse"
-            />
-          )}
-        </div>
-        <button onClick={(_) => setCloseWindow(!closeWindow)}>
-          {closeWindow ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />}
-        </button>
-      </div>
-
-      <div className={closeWindow ? 'hidden' : ''}>
-        <div className={messageArea}>
-          {initChat ? (
-            <>
-              <h2 className="font-bold pl-2 pb-2">
-                Insira seus dados para falar com Bot
-              </h2>
-              <div className="flex flex-col gap-2 px-2 pb-3">
-                <Input
-                  error={userVerific}
-                  helperText={userVerific ? 'Usuário Inválido' : ''}
-                  info={clientChat.name}
-                  label="Seu nome"
-                  handleChange={handleChange('name')}
+      return (
+        <div className={chatPreview(closeWindow)}>
+          <div className={titleChat(closeWindow)}>
+            <div className="flex gap-2 items-center">
+              <h3 className="font-bold">
+                {closeWindow ? `` : `ChatBot - Online `}
+              </h3>
+              {closeWindow ? null : (
+                <CircleIcon
+                  sx={{ fontSize: '10px', color: '#6DE040' }}
+                  className="animate-pulse"
                 />
-
-                <Input
-                  error={emailVerific}
-                  helperText={emailVerific ? 'Email inválido' : ''}
-                  info={clientChat.email}
-                  label="Seu email"
-                  handleChange={handleChange('email')}
-                />
-              </div>
-            </>
-          ) : null}
-          <button
-            className={
-              initChat
-                ? 'my-4 px-6 py-2 rounded-lg text-white font-bold  bg-navBg hover:bg-opacity-0 w-full  hover:text-navFontColor hover:border'
-                : 'hidden'
-            }
-            onClick={initiateChat}
-          >
-            Iniciar Chat
-          </button>
-
-          {initChat === false ? (
-            <>
-              {msg.map((item) => (
-                <Message
-                  key={item.id}
-                  message={item.message}
-                  user={item.user}
-                  timer={item.time}
-                />
-              ))}
-              <div ref={bottomRef} />
-            </>
-          ) : (
-            ''
-          )}
-
-          <div
-            className={
-              initChat ? 'hidden' : 'fixed bottom-1  flex gap-2 pb-[8px] w-full'
-            }
-          >
-            <input
-              disabled={disableMsg}
-              className="p-1 rounded-lg drop-shadow-md w-10/12 outline-none"
-              type="text"
-              value={textMessage}
-              onKeyUp={(e) => keyHandler(e)}
-              onChange={(e) => setTextMessage(e.target.value)}
-            />
-            <button
-              onClick={onClick}
-              className="text-navFontColor transform hover:scale-95 transition duration-100 "
-            >
-              <SendIcon />
+              )}
+            </div>
+            <button onClick={(_) => setCloseWindow(!closeWindow)}>
+              {closeWindow ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />}
             </button>
           </div>
+
+          <div className={closeWindow ? 'hidden' : ''}>
+            <div className={messageArea}>
+              {initChat ? (
+                <>
+                  <h2 className="font-bold pl-2 pb-2">
+                    Insira seus dados para falar com Bot
+                  </h2>
+                  <div className="flex flex-col gap-2 px-2 pb-3">
+                    <Input
+                      error={userVerific}
+                      helperText={userVerific ? 'Usuário Inválido' : ''}
+                      info={clientChat.name}
+                      label="Seu nome"
+                      handleChange={handleChange('name')}
+                    />
+
+                    <Input
+                      error={emailVerific}
+                      helperText={emailVerific ? 'Email inválido' : ''}
+                      info={clientChat.email}
+                      label="Seu email"
+                      handleChange={handleChange('email')}
+                    />
+                  </div>
+                </>
+              ) : null}
+              <button
+                className={
+                  initChat
+                    ? 'my-4 px-6 py-2 rounded-lg text-white font-bold  bg-navBg hover:bg-opacity-0 w-full  hover:text-navFontColor hover:border'
+                    : 'hidden'
+                }
+                onClick={initiateChat}
+              >
+                Iniciar Chat
+              </button>
+
+              {initChat === false ? (
+                <>
+                  {msg.map((item) => (
+                    <Message
+                      key={item.id}
+                      message={item.message}
+                      user={item.user}
+                      timer={item.time}
+                    />
+                  ))}
+                  <div ref={bottomRef} />
+                </>
+              ) : (
+                ''
+              )}
+
+              <div
+                className={
+                  initChat
+                    ? 'hidden'
+                    : 'fixed bottom-1  flex gap-2 pb-[8px] w-full'
+                }
+              >
+                <input
+                  disabled={disableMsg}
+                  className="p-1 rounded-lg drop-shadow-md w-10/12 outline-none"
+                  type="text"
+                  value={textMessage}
+                  onKeyUp={(e) => keyHandler(e)}
+                  onChange={(e) => setTextMessage(e.target.value)}
+                />
+                <button
+                  onClick={onClick}
+                  className="text-navFontColor transform hover:scale-95 transition duration-100 "
+                >
+                  <SendIcon />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+    }
+  });
 };
+
 export default Chatbox;
